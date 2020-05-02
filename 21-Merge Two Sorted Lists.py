9f0eca9e143183332f9d5f838dc58def59d2e448
王雪松 <wxs199327@hotmail.com>
2020-05-02 14:37:06 -0700

    finish problem 21

---
+++ 21-Merge Two Sorted Lists.py
@@ -0,0 +1,34 @@
+"""
+https://leetcode.com/problems/merge-two-sorted-lists/
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+"""
+
+
+
+# a separate head node to return is needed, then find whichever is smaller, paste it at the end of the traversing pointer
+# time complexity: O(n), space complexity:O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = ListNode()
+        pointer = head
+        while l1 and l2:
+            if l1.val < l2.val:
+                pointer.next = l1
+                l1 = l1.next
+            else:
+                pointer.next = l2
+                l2 = l2.next
+            pointer = pointer.next
+        pointer.next = l1 if l1 else l2
+        return head.next
