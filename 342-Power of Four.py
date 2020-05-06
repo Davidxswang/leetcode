eea562f9f939836512831bce78b1943320e67fba
王雪松 <wxs199327@hotmail.com>
2020-05-06 16:33:39 -0700

    finish problem 342

---
+++ 342-Power of Four.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/power-of-four/
+Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+Example 1:
+
+Input: 16
+Output: true
+Example 2:
+
+Input: 5
+Output: false
+Follow up: Could you solve it without loops/recursion?
+"""
+
+# Inspired by @h285zhao in the discussion panel
+# time complexity: O(logn) for the base conversion, space complexity: O(n)
+class Solution:
+    def isPowerOfFour(self, num: int) -> bool:
+        s = bin(num)[3:]
+        return num > 0 and ('1' not in s) and len(s)%2 == 0
