66e0bd456c4f2befd61c194b103f8354c2d20dc8
王雪松 <wxs199327@hotmail.com>
2020-05-12 20:15:01 -0700

    add quicksort based algorithm to problem 414's solution

--- 414-Third Maximum Number.py
+++ 414-Third Maximum Number.py
@@ -49,3 +49,36 @@ class Solution:
             return maxlist[-1]
         else:
             return maxlist[0]
+
+# Using divide and conquer based on quicksort
+# time complexity: O(n), space complexity: O(1)
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        s = list(set(nums))
+        if len(s) == 1:
+            return s[0]
+        if len(s) == 2:
+            return s[0] if s[0] > s[1] else s[1]
+        return self.findthirdmax(s, 0, len(s) - 1, 3)
+
+    def findthirdmax(self, nums: List[int], start: int, end: int, position: int) -> int:
+        import random
+        pivot = random.randrange(start, end + 1)
+        nums[start], nums[pivot] = nums[pivot], nums[start]
+        i = start + 1
+        j = start + 1
+        while j <= end:
+            if nums[j] > nums[start]:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+                i += 1
+            else:
+                j += 1
+        nums[i - 1], nums[start] = nums[start], nums[i - 1]
+        if i - 1 - start == position - 1:
+            return nums[i - 1]
+        else:
+            if i - start > position:
+                return self.findthirdmax(nums, start, i - 2, position)
+            if i - start < position:
+                return self.findthirdmax(nums, i, end, position - (i - start))
