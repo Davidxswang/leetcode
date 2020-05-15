1951cec90c6254c54917f215e3e38fef01d5411c
王雪松 <wxs199327@hotmail.com>
2020-05-15 11:00:12 -0700

    finish problem 190

---
+++ 190-Reverse Bits.py
@@ -0,0 +1,41 @@
+"""
+Reverse bits of a given 32 bits unsigned integer.
+
+
+
+Example 1:
+
+Input: 00000010100101000001111010011100
+Output: 00111001011110000010100101000000
+Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+Example 2:
+
+Input: 11111111111111111111111111111101
+Output: 10111111111111111111111111111111
+Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+
+
+Note:
+
+Note that in some languages such as Java, there is no unsigned integer type. In this case, both input and output will be given as signed integer type and should not affect your implementation, as the internal binary representation of the integer is the same whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above the input represents the signed integer -3 and the output represents the signed integer -1073741825.
+
+
+Follow up:
+
+If this function is called many times, how would you optimize it?
+"""
+
+# Pretty easy.
+# time complexity: O(logn), space complexity: O(n)
+# If this function is called many times, I can use a hashtable to store the result.
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        result = 0
+        i = 0
+        while i < 32:
+            result = result << 1
+            result += n & 1
+            n = n >> 1
+            i += 1
+        return result
