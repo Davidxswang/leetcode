f02ec9f96db5941d5a72e126345a86d93132ba2d
王雪松 <wxs199327@hotmail.com>
2020-05-22 10:33:26 -0700

    finish problem 566

---
+++ 566-Reshape the Matrix.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/reshape-the-matrix/
+In MATLAB, there is a very useful function called 'reshape', which can reshape a matrix into a new one with different size but keep its original data.
+
+You're given a matrix represented by a two-dimensional array, and two positive integers r and c representing the row number and column number of the wanted reshaped matrix, respectively.
+
+The reshaped matrix need to be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the 'reshape' operation with given parameters is possible and legal, output the new reshaped matrix; Otherwise, output the original matrix.
+
+Example 1:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+Output:
+[[1,2,3,4]]
+Explanation:
+The row-traversing of nums is [1,2,3,4]. The new reshaped matrix is a 1 * 4 matrix, fill it row by row by using the previous list.
+Example 2:
+Input:
+nums =
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+Output:
+[[1,2],
+ [3,4]]
+Explanation:
+There is no way to reshape a 2 * 2 matrix to a 2 * 4 matrix. So output the original matrix.
+Note:
+The height and width of the given matrix is in range [1, 100].
+The given r and c are all positive.
+"""
+
+# time complexity: O(n), space complexity: O(1)
+class Solution:
+    def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
+        if len(nums) == 0 or len(nums[0]) == 0 or len(nums) * len(nums[0]) != r * c:
+            return nums
+        row = 0
+        column = 0
+        result = []
+        for i in range(len(nums)):
+            for j in range(len(nums[i])):
+                if column == 0:
+                    result.append([])
+                result[row].append(nums[i][j])
+                column += 1
+                if column >= c:
+                    column = 0
+                    row += 1
+        return result
