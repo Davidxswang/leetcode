0ff6b91265fadce8208a11aed8216866096f42f6
Xuesong Wang <wxs199327@hotmail.com>
2020-05-28 18:53:54 -0700

    finish problem 766

---
+++ 766-Toeplitz Matrix.py
@@ -0,0 +1,68 @@
+"""
+https://leetcode.com/problems/toeplitz-matrix/
+A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same element.
+
+Now given an M x N matrix, return True if and only if the matrix is Toeplitz.
+
+
+Example 1:
+
+Input:
+matrix = [
+  [1,2,3,4],
+  [5,1,2,3],
+  [9,5,1,2]
+]
+Output: True
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
+Example 2:
+
+Input:
+matrix = [
+  [1,2],
+  [2,2]
+]
+Output: False
+Explanation:
+The diagonal "[1, 2]" has different elements.
+
+Note:
+
+matrix will be a 2D array of integers.
+matrix will have a number of rows and columns in range [1, 20].
+matrix[i][j] will be integers in range [0, 99].
+
+Follow up:
+
+What if the matrix is stored on disk, and the memory is limited such that you can only load at most one row of the matrix into the memory at once?
+What if the matrix is so large that you can only load up a partial row into the memory at once?
+"""
+
+# time complexity: O(n), space complexity: O(1)
+
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        for i in range(len(matrix[0])):
+            row, column = 0, i
+            r, c = row + 1, column + 1
+            while r < len(matrix) and c < len(matrix[0]):
+                if matrix[r][c] == matrix[row][column]:
+                    c += 1
+                    r += 1
+                else:
+                    return False
+
+        for i in range(1, len(matrix)):
+            row, column = i, 0
+            r, c = row + 1, column + 1
+            while r < len(matrix) and c < len(matrix[0]):
+                if matrix[r][c] == matrix[row][column]:
+                    r += 1
+                    c += 1
+                else:
+                    return False
+
+        return True
