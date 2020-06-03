8699e803b2a7b71c83656b9970e51ee43676cbd9
Xuesong Wang <wxs199327@hotmail.com>
2020-06-03 05:06:30 -0700

    finish problem 937

---
+++ 937-Reorder Data in Log Files.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/problems/reorder-data-in-log-files/
+You have an array of logs.  Each log is a space delimited string of words.
+
+For each log, the first word in each log is an alphanumeric identifier.  Then, either:
+
+Each word after the identifier will consist only of lowercase letters, or;
+Each word after the identifier will consist only of digits.
+We will call these two varieties of logs letter-logs and digit-logs.  It is guaranteed that each log has at least one word after its identifier.
+
+Reorder the logs so that all of the letter-logs come before any digit-log.  The letter-logs are ordered lexicographically ignoring identifier, with the identifier used in case of ties.  The digit-logs should be put in their original order.
+
+Return the final order of the logs.
+
+
+
+Example 1:
+
+Input: logs = ["dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"]
+Output: ["let1 art can","let3 art zero","let2 own kit dig","dig1 8 1 5 1","dig2 3 6"]
+
+
+Constraints:
+
+0 <= logs.length <= 100
+3 <= logs[i].length <= 100
+logs[i] is guaranteed to have an identifier, and a word after the identifier.
+"""
+
+# time complexity: O(nlogn), space complexity: O(1)
+
+class Solution:
+    def reorderLogFiles(self, logs: List[str]) -> List[str]:
+        def sort(string):
+            array = string.split()
+            return ' '.join(array[1:] + array[:1])
+
+        letterlog = []
+        digitlog = []
+        for log in logs:
+            if self.isdigitlog(log):
+                digitlog.append(log)
+            else:
+                letterlog.append(log)
+        letterlog.sort(key=sort)
+        return letterlog + digitlog
+
+    def isdigitlog(self, string):
+        beforeidentifier = True
+        i = 0
+        while i < len(string):
+            if beforeidentifier:
+                if string[i] == ' ' and string[i - 1] != ' ':
+                    beforeidentifier = False
+            else:
+                if 'a' <= string[i] <= 'z':
+                    return False
+                else:
+                    return True
+            i += 1
+
+
