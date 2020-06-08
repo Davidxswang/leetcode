a4ff0e56db0a22607889a57589798650ad6406ee
Xuesong Wang <wxs199327@hotmail.com>
2020-06-07 23:29:04 -0700

    finish problem 1200

---
+++ easy/1200-Minimum Absolute Difference.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/minimum-absolute-difference/
+Given an array of distinct integers arr, find all pairs of elements with the minimum absolute difference of any two elements.
+
+Return a list of pairs in ascending order(with respect to pairs), each pair [a, b] follows
+
+a, b are from arr
+a < b
+b - a equals to the minimum absolute difference of any two elements in arr
+
+
+Example 1:
+
+Input: arr = [4,2,1,3]
+Output: [[1,2],[2,3],[3,4]]
+Explanation: The minimum absolute difference is 1. List all pairs with difference equal to 1 in ascending order.
+Example 2:
+
+Input: arr = [1,3,6,10,15]
+Output: [[1,3]]
+Example 3:
+
+Input: arr = [3,8,-10,23,19,-4,-14,27]
+Output: [[-14,-10],[19,23],[23,27]]
+
+
+Constraints:
+
+2 <= arr.length <= 10^5
+-10^6 <= arr[i] <= 10^6
+"""
+
+# time complexity: O(nlogn), space complexity: O(1)
+
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        arr.sort()
+        result = [[arr[0], arr[1]]]
+        for i in range(1, len(arr)-1):
+            if arr[i+1] - arr[i] == result[-1][1] - result[-1][0]:
+                result.append([arr[i],arr[i+1]])
+            elif arr[i+1] - arr[i] < result[-1][1] - result[-1][0]:
+                result = [[arr[i], arr[i+1]]]
+        return result
