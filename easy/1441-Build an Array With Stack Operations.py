bd3247581ef3de1d468c0743623186d040720c8e
Xuesong Wang <wxs199327@hotmail.com>
2020-06-08 23:04:37 -0700

    finish problem 1441

---
+++ easy/1441-Build an Array With Stack Operations.py
@@ -0,0 +1,63 @@
+"""
+https://leetcode.com/problems/build-an-array-with-stack-operations/
+Given an array target and an integer n. In each iteration, you will read a number from  list = {1,2,3..., n}.
+
+Build the target array using the following operations:
+
+Push: Read a new element from the beginning list, and push it in the array.
+Pop: delete the last element of the array.
+If the target array is already built, stop reading more elements.
+You are guaranteed that the target array is strictly increasing, only containing numbers between 1 to n inclusive.
+
+Return the operations to build the target array.
+
+You are guaranteed that the answer is unique.
+
+ 
+
+Example 1:
+
+Input: target = [1,3], n = 3
+Output: ["Push","Push","Pop","Push"]
+Explanation: 
+Read number 1 and automatically push in the array -> [1]
+Read number 2 and automatically push in the array then Pop it -> [1]
+Read number 3 and automatically push in the array -> [1,3]
+Example 2:
+
+Input: target = [1,2,3], n = 3
+Output: ["Push","Push","Push"]
+Example 3:
+
+Input: target = [1,2], n = 4
+Output: ["Push","Push"]
+Explanation: You only need to read the first 2 numbers and stop.
+Example 4:
+
+Input: target = [2,3,4], n = 4
+Output: ["Push","Pop","Push","Push","Push"]
+ 
+
+Constraints:
+
+1 <= target.length <= 100
+1 <= target[i] <= 100
+1 <= n <= 100
+target is strictly increasing.
+"""
+# time complexity: O(n), space complexity: O(1)
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        result = []
+        i = 1
+        pointer = 0
+        while i <= n and pointer < len(target):
+            if i == target[pointer]:
+                result.append("Push")
+                i += 1
+                pointer += 1
+            else:
+                result.append("Push")
+                result.append("Pop")
+                i += 1
+        return result
